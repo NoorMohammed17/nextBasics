f1385f26e528790622ed0fa56200d924856967ae
NoorMohammed17 <noormuhammads1797@gmail.com>
2023-09-08 17:57:56 +0530

    adding paths and nested paths

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "0f8aab5de7819bd8"; }
+/******/ 		__webpack_require__.h = function() { return "bcc3660bd5a6346f"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
